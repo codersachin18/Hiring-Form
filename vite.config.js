51c1c6fefe2eca6bafe52686f9fe941aebf4eb35
{codersachin18} <sachinraj18sj@gmail.com>
2025-01-25 12:34:58 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Hiring-Form-", // Repository name for GitHub Pages
+  build: {
+    outDir: 'dist', // Specify the output directory
+  },
 })
